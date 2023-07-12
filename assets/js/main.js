1207b85f03aef0ddff151abc4c890dd1df113ed2
songhee24 <akaratel61@gmail.com>
2023-07-13 04:57:12 +0600

    CHANGE BACKGROUND HEADER WHILE SCROLL EVENT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
-
 
 /*=============== HOME SWIPER ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
 
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== NEW SWIPER ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
