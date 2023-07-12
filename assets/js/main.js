6ec9b2e30c4563ae6a21567de5239fb60fd17724
songhee24 <akaratel61@gmail.com>
2023-07-13 05:00:18 +0600

    CHANGE BACKGROUND HEADER WHILE SCROLL EVENT #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  if (this.scrollY >= 50) {
+  if (this.scrollY >= 40) {
     header.classList.add("scroll-header");
   } else {
     header.classList.remove("scroll-header");
